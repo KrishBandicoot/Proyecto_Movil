f3fa1bd08373685f7d2cf35be315af0e77f25ecb
KrishBandicoot <cristian.lizama.diaz2014@gmail.com>
2025-10-13 01:52:41 -0300

    Delete Kkarhua directory

--- Kkarhua/app/src/main/java/com/example/kkarhua/viewmodel/ProductListViewModel.kt
+++
@@ -1,4 +0,0 @@
-package com.example.kkarhua.viewmodel
-class ProductListViewModel(private val repo: ProductRepository) : ViewModel() {
-    val products: LiveData<List<Product>> = repo.getAllProducts().asLiveData()
-}
